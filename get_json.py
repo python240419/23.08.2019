d67160e9b1de528f0f8947f5d20cd446a6c1ceeb
python240419 <51232248+python240419@users.noreply.github.com>
2019-08-23 12:20:01 +0300

    Create get_json.py

---
+++ get_json.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+resp = requests.get\
+    ("https://jsonplaceholder.typicode.com/todos/1")
+print(f'Status code = {resp.status_code}')
+#print(f'Header = {resp.headers}')
+#print(resp.content)
+
+d = json.loads(resp.content)
+class Todos:
+    def __init__(self, d):
+        self.__dict__ = d
+    def __str__(self):
+        result = ""
+        for k, v in self.__dict__.items():
+            result += k
+            result += " : "
+            result += str(v)
+            result += '\n'
+        return result
+
+t = Todos(d)
+print(t.userId)
