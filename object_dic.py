8ba4c2438639e5c8e88592b25a3dfc25c0cbb44d
python240419 <51232248+python240419@users.noreply.github.com>
2019-08-23 10:14:24 +0300

    Update object_dic.py

--- object_dic.py
+++ object_dic.py
@@ -2,11 +2,16 @@
 #import json
 
 class Person:
-    def __init__(self, name, age,
-                 city):
-        self.name = name
-        self.age = age
-        self.city = city
+    def __init__(self, d):
+        # 1
+        self.__dict__ = d
+        # 2
+        for k, v in d.items():
+            self.__dict__[k] = v
+
+        #self.name = name
+        #self.age = age
+        #self.city = city
     def __str__(self):
         #return f'name : {self.name}
         # age : {self.age}'
@@ -18,11 +23,17 @@ class Person:
             result += '\n'
         return result
 
-# moshe = Person('Moshe', 37, "Ashdod")
-initParam = {'name' : 'Moshe', 'age' : '37', 'city' : 'Ashdod'}
+#moshe = Person('Moshe', 37, "Ashdod")
+initParam = {'name' : 'Moshe',
+    'age' : '37',
+'city' : 'Ashdod'}
 
 # this needs to work
-moshe = person(initParam)
+moshe = Person(initParam)
+moshe.__dict__['hi'] = 'hello'
+print(moshe.hi) # 'hello'
+moshe.hi = 'hello'
 
-
+print(moshe)
+#print(moshe.__dict__)
 
